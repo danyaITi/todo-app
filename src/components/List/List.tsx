4bcf47511988ba3b9737a32d2c5bca83dd890a8f
danyaITi <danil.lenskiy.1997@mail.ru>
2022-07-31 00:59:18 +0900

    fixed error

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { TodoProps } from "../Main"
 
 type ListProps = {
@@ -9,6 +9,7 @@ type ListProps = {
 }
 
 const List: React.FC<ListProps> = ({handleToggle, todos, statusFilter, removeTodo}) => {
+    const [checked, setChecked] = useState(false)
     
 
     const sortTodos = todos.filter((item)=>{
@@ -21,12 +22,16 @@ const List: React.FC<ListProps> = ({handleToggle, todos, statusFilter, removeTod
         }
         
     })
+
+
     return (
         <ul className="list-group mb-0">
-            {sortTodos.map((item)=> <li key={item.id}  className={item.status === true ? 'list-group-item d-flex align-items-center border-0 mb-2 rounded text-decoration-line-through' : 'list-group-item d-flex align-items-center border-0 mb-2 rounded'} style={{backgroundColor: '#f4f6f7'}}>
-            <input onClick={()=>handleToggle(item.id)} className="form-check-input me-2" checked={item.status} type="checkbox" value="Incompleted" aria-label="..."  />
-            <div>{item.value}</div>
-            <button type="button" className="btn btn-danger" onClick={()=>removeTodo(item.id)}>Delete</button>
+            {sortTodos.map((item)=> <li key={item.id}  className={item.status === true ? 'd-flex justify-content-between list-group-item d-flex align-items-center border-0 mb-2 rounded text-warning bg-dark bg-gradient' : 'd-flex justify-content-between list-group-item d-flex align-items-center border-1 mb-2 rounded'} style={{backgroundColor: '#f4f6f7'}}>
+            <div>
+                <input onClick={()=>handleToggle(item.id)} className="form-check-input me-2" checked={item.status} onChange={e => setChecked(e.target.checked)} type="checkbox" value="Incompleted" aria-label="..."  />
+                <span className="mx-2">{item.value}</span>
+            </div>
+            <button type="button" className="btn btn-danger mx-3" onClick={()=>removeTodo(item.id)}>Delete</button>
             </li>)}
         </ul>
     )
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -19,24 +19,26 @@ const Main: React.FC = () => {
     }, [todos]);
     
 
-    const addTodo = (e: MouseEvent) => {
-        e.preventDefault()
+    const addTodo = (event: MouseEvent) => {
+        event.preventDefault()
         if(value){
             const newItem = {
                 id: todos.length ,
                 value,
                 status: false
             }
-    
+        
             setTodos([...todos,newItem])
         }
         setValue('')
+       
     }
 
     const handleToggle = (id: number) => {
        setTodos([
         ...todos.map((todo)=> todo.id === id ? {...todo, status: !todo.status } : {...todo })
        ])
+        
        
     }
 
@@ -51,17 +53,16 @@ const Main: React.FC = () => {
 
 
     return (
-        <section className="vh-100 gradient-custom">
+        <section className="vh-90 gradient-custom">
             <div className="container py-5 h-100">
                 <div className="row d-flex justify-content-center align-items-center h-100">
                     <div className="col col-xl-10">
                         <div className="card">
                             <div className="card-body p-5">
                                 <form className="d-flex justify-content-center align-items-center mb-4">
-                                    <div className="mb-3 d-flex justify-content-center">
-                                        <label htmlFor="formGroupExampleInput" className="form-label"></label>
-                                        <input type="text" style={{width: '24em'}} value={value} onChange={(e)=>setValue(e.target.value)} className="form-control" id="formGroupExampleInput" placeholder="Enter todo"/>
-                                        <button type="button" className="btn btn-dark ms-2" onClick={addTodo}>Add</button>
+                                    <div className="input-group input-group-sm mb-3">
+                                        <input type="text" className="form-control p-2" value={value} onChange={(e)=>setValue(e.target.value)} placeholder="Enter todo" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                        <button type="button" className="btn btn-warning mx-2 px-4"  onClick={addTodo}><span className="h6">Add</span></button>
                                     </div>
                                 </form>
                                 <Sort statusFilter={statusFilter} changeStatus={changeStatus}/>
